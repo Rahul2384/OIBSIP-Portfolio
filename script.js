131b842ddcad37948e272aebfcdc00a65036676e
Rahul Kasula <rahulkasula23@gmail.com>
2023-09-01 10:55:23 +0530

    Version-1

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["3rd year CSE student at Vasavi college of Engineering.","Programmer","Problem Solver","Web Developer"],
+    typeSpeed: 30,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop:true
+});
